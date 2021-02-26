0ddf1e3ddaa60bd9351486042e8401f6ec11b838
swordpepi <71564854+swordpepi@users.noreply.github.com>
2021-02-26 12:46:57 +0200

    added css and stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -9,6 +9,15 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/discord")
+def discord():
+    return redirect('https://discord.gg/myjXrkH2fu')
+
+@app.route("/forum")
+def forum():
+    return render_template("forum.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
